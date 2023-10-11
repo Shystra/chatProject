041b30ccfbc1e0bcac3273b078e34b8976d3a852
Shystra <jlsp.2019@hotmail.com>
2023-10-11 20:12:39 -0300

    config in tailwind

--- realtime-chat/tailwind.config.js
+++ realtime-chat/tailwind.config.js
@@ -5,8 +5,25 @@ export default {
     fontFamily: {
       sans: [ 'Open Sans', 'sans-serif' ],
     },
-    extend: {},
+    extend: {
+      colors:{
+        'blue': '#023AFF',
+        'blue-100': "#03009206",
+        'blue-200': "#023AFF08",
+        'blue-300': "#023AFF15",
+        green: "#5CDE88",
+        'gray-100': "#E1E1E1",
+        'gray-200': "#999999",
+        'gray-300': "#666666",
+        black: '#333333',
+      },
+      borderRadius: {
+        rounded:{
+          100: '10px',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
 
